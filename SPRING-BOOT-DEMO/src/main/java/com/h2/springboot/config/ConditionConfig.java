2d235b38fdb510d65efa86062611da7f04c7c96e
HuHao <huhao@chinadaas.com>
2020-09-04 16:50:03 +0800

    @Value获取properties文件中文乱码问题解决

--- SPRING-BOOT-DEMO/src/main/java/com/h2/springboot/config/ConditionConfig.java
+++ SPRING-BOOT-DEMO/src/main/java/com/h2/springboot/config/ConditionConfig.java
@@ -15,10 +15,13 @@ import java.util.Properties;
  * 条件装配bean配置文件
  * Created by: huhao
  * Date: 2020/9/3
+ * 解决properties文件中文乱码:
+ * 1:指定encoding为utf-8
+ * 2:settings->editor->file encodings中设置为utf-8,并勾选Transparent native-to-ascii conversion
  */
 @Configuration//代表这是一个java配置文件,spring容器会根据它来生成ioc容器装配bean
 @ComponentScan(basePackages = {"com.h2.springboot.*"})
-@PropertySource(value = {"classpath:mycondition.properties"})
+@PropertySource(value = {"classpath:mycondition.properties"},encoding = "UTF-8")
 public class ConditionConfig {
 
     @Bean(name = "myCondition")//加上Bean后,方法中的参数必须要注入属性对应的值
--- SPRING-BOOT-DEMO/src/main/java/com/h2/springboot/properties/DataBaseProperties.java
+++ SPRING-BOOT-DEMO/src/main/java/com/h2/springboot/properties/DataBaseProperties.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
